377a4b52cc6dcfc54dabe72f746ef6cb57ca0c38
Pau Costa Ferrer <mico@micodev.es>
2021-05-17 22:01:06 +0200

    initial commit

---
+++ src/ExampleObjects/Objects.js
@@ -0,0 +1,18 @@
+let inventories = {
+  id: "uuid",
+  name: "name",
+  location: "location",
+  "date-due": "ddmmyyyy",
+  completed: false,
+  assignedTo: "userId ",
+  items: [
+    {
+      ref: "ref",
+      description: "desc",
+      counts: [
+        { id: "uuid", date: "ddmmyyhhss", value: "float", userId: "userID" },
+      ],
+      finalCount: "float",
+    },
+  ],
+};
